80f73500e4fd399aa9ddd759259666bd018b1070
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-09-16 12:51:39 +0400

    second commit

---
+++ day 02/homework/lesson 3.py
@@ -0,0 +1,10 @@
+#variable = ცვლადი
+# variable value = ცვლადის მნიშვნელობა
+#parentheses - ფჩხილები
+#quotes -ბრჭყალები
+
+name = "alexandre"
+surname = "jimshiashvili"
+
+print (name)
+print (surname)
