928381f84861189a62aa6f3845d7717bd3bde8dc
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-09-23 13:49:22 +0400

    second commit

---
+++ day 03/homework/homwork 4.py
@@ -0,0 +1,11 @@
+age = 13
+print (age + 5)
+
+age2 = 37
+
+print (age + age2 + 5)
+
+name = "giorgi"
+name2 = "giorgi"
+# ორივე გიორგი იმიტომ რომ ჩემს მეგობარსაც გიორგი ჰქვია
+print (name + " da " + name2)
