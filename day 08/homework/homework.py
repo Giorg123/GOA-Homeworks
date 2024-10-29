bf1e005ba70dd47363cb1789e0c10c6a4cca9b09
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-10-29 20:56:48 +0400

    second commit

---
+++ day 08/homework/homework.py
@@ -0,0 +1,17 @@
+print (9 > 1)
+print (1 < 3)
+print (1 == 1)
+
+#age = int(input("please enter your age"))
+#print (age < 3)
+
+#surname = input("please enter your name: ")
+#print (surname == "keleptrishvili")
+#age2 = int(input("please enter your age: "))
+#print (age2 >= 18)
+
+print (True or False)
+print ("name" == "name")
+print ( 123 == "123")
+print (5 >= 5)
+
