2a60013d67fdba660e1d35808b77b8ad1cbbc5ac
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-11-12 19:00:48 +0400

    second commit

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,15 @@
+#for i in range(49,150,2):
+#    print (i)
+
+#name = input("please enter your name: ")
+#for G in name:
+##    print (G)
+
+
+#M = 500
+#while M < 700:
+#    print (M)
+#    M += 2
+K = 50
+while K < 0:
+    print (K)
---
+++ day 10/homework/home.py
@@ -0,0 +1,13 @@
+#age = (input("please enter your age: "))
+#for i in age:
+#    print (i)
+
+
+#M = 1
+#while M < 10:
+#    print(M)
+#    M = M + 2
+
+
+for i in range(2,20,2):
+    print (i)
