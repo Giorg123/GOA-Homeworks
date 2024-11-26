294fc172908a93f33921ef88c52811983a241fc1
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-11-26 19:58:36 +0400

    second commit

---
+++ day 12/classwork/cw.py
@@ -0,0 +1,3 @@
+numbers = ["a", "b" "c" "d" "e" "1" "2" "3" "4" "5"]
+jemala_index = int(input("enter your number: "))
+print (numbers[jemala_index])
---
+++ day 12/homework/hw.py
@@ -0,0 +1,29 @@
+numbers = int(input("enter number"))
+if numbers % 2 == 0:
+    print ("even")
+else:
+    print ("odd")
+
+
+
+
+for i in range(2,50,2):
+    print (i)
+
+
+
+
+
+arr = [1,2,3,4,5,6,12,23,46,12,9,2]
+for i in arr:
+    if i > 5:
+        print(i)
+
+
+
+ad = [1,2,3,4,5,6,7,8,9,10]
+
+print(arr[0] + arr[-1]) #11
+print(arr[0] - arr[-1]) #-9
+print(arr[0] * arr[-1]) #10
+print(arr[0] / arr[-1]) #0.1
