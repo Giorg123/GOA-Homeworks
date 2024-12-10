3882d4a1d1bb572518a947c43d2a362ca7f31499
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-12-10 19:08:46 +0400

    second commit

---
+++ day 14/classwork/cw.py
@@ -0,0 +1,7 @@
+#numbers = ("1", "2")
+#print(numbers[0:3])
+
+name = input ("plase enter name ")
+user = "giorgi"
+print (name[0:3]) + (user[3:])
+
---
+++ day 14/homework/hw.py
@@ -0,0 +1,13 @@
+weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print(weeks[0])
+print (weeks[-1])
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,]
+print (nums[0:13:3])
+print (nums[3:9])
+
+
+
+
+numbers = [1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
+print (numbers[0:19:5])
