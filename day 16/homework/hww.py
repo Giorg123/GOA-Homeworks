7427ada9e9859bdfbda446b1394a3be0e5da1c7d
Giorgi123 <g.keleptrishvili.10gmail.com>
2024-12-24 20:39:13 +0400

    second commit

---
+++ day 16/homework/hww.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (numbers[1:11:2])
+
+
+name = input('please enter your name: ' )
+print (name.lower())
+
+
+surname = "keleptrishvili"
+print (surname.upper())
+
+
+ABC = "hmmmmm"
+print (ABC.capitalize())
