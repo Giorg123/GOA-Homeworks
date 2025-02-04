af59772ff9b2f9cc1da387cf7d5ee15250aae21b
giorgikeleptrishvili <lonycontentcreator@gmail.com>
2025-02-04 19:18:20 +0400

    commit

---
+++ day 22/classwork/cw.py
@@ -0,0 +1,19 @@
+def funqcia(a,b):
+    print (a - b)
+
+print (funqcia (1,2))
+
+
+def winadadeba (g,h):
+    print (g + h)
+
+
+print (winadadeba("ra","ra"))
+
+
+def list (o,k):
+    return [1, 2, 3, 4, 5, 6, 7, 8,]
+
+
+list (2,4)
+    
